cda4d84f55c100d801b9753cb3bb443007ca6597
Eduard <Eduardsipad@yandex.ru>
2025-09-27 09:48:48 +0300

    initian commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .primary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsetsGeometry.only(bottom: 16),
+              child: Text("Павлов Эдуард Вадимович"),
+            ),
+            Padding(
+              padding: EdgeInsetsGeometry.only(bottom: 16),
+              child: Text('ИКБО-06-22'),
+            ),
+            Text('22И1754')
+          ],
+        ),
+      ),
+    );
+  }
+}
