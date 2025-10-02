da08bd218488a8a85a3a2cb3fc0aab25c8e06361
Eduard <Eduardsipad@yandex.ru>
2025-10-02 20:57:40 +0300

    Модернизация файла main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,69 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyStateFulWidget(),
-    );
-  }
-}
-
-class MyStateLessWidget extends StatelessWidget {
-  const MyStateLessWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FilledButton(
-          onPressed: () {  },
-          style: ButtonStyle(
-              backgroundColor: WidgetStatePropertyAll(Colors.red),
-              shape: WidgetStatePropertyAll(
-                  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0)
-                  )
-              )
-          ),
-          child: Text("Click me"),
-        )
-      )
-    );
-  }
-}
-
-
-class MyStateFulWidget extends StatefulWidget {
-  const MyStateFulWidget({super.key});
-
-  @override
-  State<MyStateFulWidget> createState() => _MyStateFulWidgetState();
-}
-
-class _MyStateFulWidgetState extends State<MyStateFulWidget> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: FilledButton(
-              onPressed: () { _incrementCounter(); },
-              style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Colors.red),
-                  shape: WidgetStatePropertyAll(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16.0)
-                      )
-                  )
-              ),
-              child: Text("Нажато $_counter раз"),
-            )
-        )
+      home: const HomeScreen(),
     );
   }
 }
