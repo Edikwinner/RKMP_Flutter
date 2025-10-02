eb035ef967e5be25c5850f4968278e978030f221
Eduard <Eduardsipad@yandex.ru>
2025-10-02 19:39:32 +0300

    Создание собственного Stateful и Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,70 +14,69 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyStateFulWidget(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MyStateLessWidget extends StatelessWidget {
+  const MyStateLessWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .primary,
-        title: Text(title),
-      ),
       body: Center(
-        child: Container(
-          color: Colors.yellow,
-          padding: EdgeInsetsGeometry.symmetric(vertical: 16),
-          child:  Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsetsGeometry.only(right: 16),
-                child: Text(
-                  "Павлов Эдуард Вадимович",
-                  style: TextStyle(color: Colors.green),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsetsGeometry.only(right: 16),
-                child: Text(
-                    'ИКБО-06-22',
-                    style: TextStyle(color: Colors.green)
-                ),
-              ),
-              Padding(
-                padding: EdgeInsetsGeometry.only(right: 16),
-                child: Text(
-                  '22И1754',
-                  style: TextStyle(color: Colors.green),
-                ),
-              ),
-              FilledButton(
-                onPressed: () {  },
-                style: ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(Colors.red),
-                    shape: WidgetStatePropertyAll(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16.0)
-                        )
-                    )
-                ),
-                child: Text("Click me"),
+        child: FilledButton(
+          onPressed: () {  },
+          style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.red),
+              shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0)
+                  )
               )
-            ],
           ),
+          child: Text("Click me"),
         )
       )
+    );
+  }
+}
+
+
+class MyStateFulWidget extends StatefulWidget {
+  const MyStateFulWidget({super.key});
+
+  @override
+  State<MyStateFulWidget> createState() => _MyStateFulWidgetState();
+}
+
+class _MyStateFulWidgetState extends State<MyStateFulWidget> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: FilledButton(
+              onPressed: () { _incrementCounter(); },
+              style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.red),
+                  shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0)
+                      )
+                  )
+              ),
+              child: Text("Нажато $_counter раз"),
+            )
+        )
     );
   }
 }
