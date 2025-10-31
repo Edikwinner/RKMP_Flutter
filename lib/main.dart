8c2b39d3f886da4333153b8ca5238b815e5ba803
Eduard <Eduardsipad@yandex.ru>
2025-10-31 21:02:03 +0300

    Добавление роутера маршрутизированной навигации

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/features/estates/state/estate_container.dart';
+
+import 'features/estates/models/estate_store.dart';
+import 'features/navigation/app_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final EstateStore estateStore = EstateStore();
+
+  late final router = createRouter(estateStore: estateStore);
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const EstateContainer(),
+    return MaterialApp.router(
+      title: 'Учет собственности',
+      routerConfig: router,
     );
   }
 }
