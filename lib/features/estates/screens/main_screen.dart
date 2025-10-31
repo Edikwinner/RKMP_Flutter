d9bd918c307754cf66b02106aa61d3828158ac68
Eduard <Eduardsipad@yandex.ru>
2025-10-31 21:05:37 +0300

    Добавление вертикальной маршрутизированной навигации

--- lib/features/estates/screens/main_screen.dart
+++ lib/features/estates/screens/main_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-
-import '../models/estate_model.dart';
+import 'package:flutter_app/features/estates/models/estate_store.dart';
 
 class MainScreen extends StatelessWidget {
-  final List<EstateModel> estates;
+  final EstateStore estateStore;
 
   final Function() onCar;
   final Function() onFlat;
@@ -13,7 +12,7 @@ class MainScreen extends StatelessWidget {
 
   const MainScreen({
     super.key,
-    required this.estates,
+    required this.estateStore,
     required this.onCar,
     required this.onFlat,
     required this.onHouse,
@@ -23,116 +22,124 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Учет собственности"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsetsGeometry.only(bottom: 8),
-              child: Text(
-                "Павлов Эдуард Вадимович",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.only(bottom: 8),
-              child: Text('ИКБО-06-22', style: TextStyle(color: Colors.black)),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.only(bottom: 16),
-              child: Text('22И1754', style: TextStyle(color: Colors.black)),
-            ),
+    return AnimatedBuilder(
+      animation: estateStore,
+      builder: (context, _) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text("Учет собственности"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsetsGeometry.only(bottom: 8),
+                  child: Text(
+                    "Павлов Эдуард Вадимович",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsGeometry.only(bottom: 8),
+                  child: Text(
+                    'ИКБО-06-22',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsGeometry.only(bottom: 16),
+                  child: Text('22И1754', style: TextStyle(color: Colors.black)),
+                ),
 
-            Padding(
-              padding: EdgeInsetsGeometry.only(bottom: 16),
-              child: Text(
-                'Общая стоимость собственности: ${estates.fold(0, (sum, item) => sum + item.cost)} ₽',
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.only(bottom: 16),
-              child: FilledButton(
-                onPressed: onCar,
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Colors.red),
-                  shape: WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
+                Padding(
+                  padding: EdgeInsetsGeometry.only(bottom: 16),
+                  child: Text(
+                    'Общая стоимость собственности: ${estateStore.estates.fold(0, (sum, item) => sum + item.cost)} ₽',
+                    style: TextStyle(color: Colors.black),
                   ),
                 ),
-                child: Text("Машины"),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.only(bottom: 16),
-              child: FilledButton(
-                onPressed: onFlat,
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Colors.orange),
-                  shape: WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0),
+                Padding(
+                  padding: EdgeInsetsGeometry.only(bottom: 16),
+                  child: FilledButton(
+                    onPressed: onCar,
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.red),
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
                     ),
+                    child: Text("Машины"),
                   ),
                 ),
-                child: Text("Квартиры"),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.only(bottom: 16),
-              child: FilledButton(
-                onPressed: onHouse,
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Colors.amber),
-                  shape: WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0),
+                Padding(
+                  padding: EdgeInsetsGeometry.only(bottom: 16),
+                  child: FilledButton(
+                    onPressed: onFlat,
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.orange),
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
                     ),
+                    child: Text("Квартиры"),
                   ),
                 ),
-                child: Text("Дома"),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.only(bottom: 16),
-              child: FilledButton(
-                onPressed: onGarage,
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Colors.lime),
-                  shape: WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0),
+                Padding(
+                  padding: EdgeInsetsGeometry.only(bottom: 16),
+                  child: FilledButton(
+                    onPressed: onHouse,
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.amber),
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
                     ),
+                    child: Text("Дома"),
                   ),
                 ),
-                child: Text("Гаражи"),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.only(bottom: 16),
-              child: FilledButton(
-                onPressed: onMoney,
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Colors.green),
-                  shape: WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0),
+                Padding(
+                  padding: EdgeInsetsGeometry.only(bottom: 16),
+                  child: FilledButton(
+                    onPressed: onGarage,
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.lime),
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
                     ),
+                    child: Text("Гаражи"),
                   ),
                 ),
-                child: Text("Деньги"),
-              ),
+                Padding(
+                  padding: EdgeInsetsGeometry.only(bottom: 16),
+                  child: FilledButton(
+                    onPressed: onMoney,
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.green),
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
+                    ),
+                    child: Text("Деньги"),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
