f9ecbcdbd974e7a6d8843c61859bd6e3452f5e37
Eduard <Eduardsipad@yandex.ru>
2025-11-06 14:31:36 +0300

    Исправление роутера маршрутизированной навигации

--- lib/features/navigation/app_router.dart
+++ lib/features/navigation/app_router.dart
@@ -1,12 +1,11 @@
 import 'package:flutter_app/features/estates/models/estate_store.dart';
 import 'package:flutter_app/features/estates/screens/cars_screen.dart';
 import 'package:flutter_app/features/estates/screens/flats_screen.dart';
+import 'package:flutter_app/features/estates/screens/garages_screen.dart';
 import 'package:flutter_app/features/estates/screens/houses_screen.dart';
 import 'package:flutter_app/features/estates/screens/money_screen.dart';
 import 'package:go_router/go_router.dart';
 
-import '../estates/screens/garages_screen.dart';
-import '../estates/screens/home_shell.dart';
 import '../estates/screens/main_screen.dart';
 
 GoRouter createRouter({required EstateStore estateStore}) {
@@ -16,110 +15,82 @@ GoRouter createRouter({required EstateStore estateStore}) {
     routes: [
       GoRoute(
         path: '/',
-        builder: (context, state) => MainScreen(
+        builder: (context, state) => MainScreen(estateStore: estateStore),
+      ),
+
+      GoRoute(
+        path: '/cars',
+        builder: (context, state) => CarsScreen(
           estateStore: estateStore,
-          onCar: () {
-            context.go('/cars');
+          onBack: () { context.go('/'); },
+          onAddEstate: (estate) {
+            estateStore.add(estate);
           },
-          onFlat: () {
-            context.go('/flats');
+          onDeleteEstate: (id) {
+            estateStore.remove(id);
           },
-          onHouse: () {
-            context.go('/houses');
-          },
-          onGarage: () {
-            context.go('/garages');
-          },
-          onMoney: () {
-            context.go('/money');
-          },
+          onEstateClick: (id) {},
         ),
       ),
 
-      ShellRoute(
-        builder: (context, state, child) => HomeShell(child: child),
-        routes: [
-          GoRoute(
-            path: '/cars',
-            builder: (context, state) => CarsScreen(
-              estateStore: estateStore,
-              onBack: () {
-                context.go('/');
-              },
-              onAddEstate: (estate) {
-                estateStore.add(estate);
-              },
-              onDeleteEstate: (id) {
-                estateStore.remove(id);
-              },
-              onEstateClick: (id) {},
-            ),
-          ),
-          GoRoute(
-            path: '/flats',
-            builder: (context, state) => FlatsScreen(
-              estateStore: estateStore,
-              onBack: () {
-                context.go('/');
-              },
-              onAddEstate: (estate) {
-                estateStore.add(estate);
-              },
-              onDeleteEstate: (id) {
-                estateStore.remove(id);
-              },
-              onEstateClick: (id) {},
-            ),
-          ),
-          GoRoute(
-            path: '/houses',
-            builder: (context, state) => HousesScreen(
-              estateStore: estateStore,
-              onBack: () {
-                context.go('/');
-              },
-              onAddEstate: (estate) {
-                estateStore.add(estate);
-              },
-              onDeleteEstate: (id) {
-                estateStore.remove(id);
-              },
-              onEstateClick: (id) {},
-            ),
-          ),
-          GoRoute(
-            path: '/garages',
-            builder: (context, state) => GaragesScreen(
-              estateStore: estateStore,
-              onBack: () {
-                context.go('/');
-              },
-              onAddEstate: (estate) {
-                estateStore.add(estate);
-              },
-              onDeleteEstate: (id) {
-                estateStore.remove(id);
-              },
-              onEstateClick: (id) {},
-            ),
-          ),
-          GoRoute(
-            path: '/money',
-            builder: (context, state) => MoneyScreen(
-              estateStore: estateStore,
-              onBack: () {
-                context.go('/');
-              },
-              onAddEstate: (estate) {
-                estateStore.add(estate);
-              },
-              onDeleteEstate: (id) {
-                estateStore.remove(id);
-              },
-              onEstateClick: (id) {},
-            ),
-          ),
-        ],
+      GoRoute(
+        path: '/flats',
+        builder: (context, state) => FlatsScreen(
+          estateStore: estateStore,
+          onBack: () { context.go('/'); },
+          onAddEstate: (estate) {
+            estateStore.add(estate);
+          },
+          onDeleteEstate: (id) {
+            estateStore.remove(id);
+          },
+          onEstateClick: (id) {},
+        ),
+      ),
+
+      GoRoute(
+        path: '/houses',
+        builder: (context, state) => HousesScreen(
+          estateStore: estateStore,
+          onBack: () { context.go('/'); },
+          onAddEstate: (estate) {
+            estateStore.add(estate);
+          },
+          onDeleteEstate: (id) {
+            estateStore.remove(id);
+          },
+          onEstateClick: (id) {},
+        ),
+      ),
+
+      GoRoute(
+        path: '/garages',
+        builder: (context, state) => GaragesScreen(
+          estateStore: estateStore,
+          onBack: () { context.go('/'); },
+          onAddEstate: (estate) {
+            estateStore.add(estate);
+          },
+          onDeleteEstate: (id) {
+            estateStore.remove(id);
+          },
+          onEstateClick: (id) {},
+        ),
+      ),
+
+      GoRoute(
+        path: '/money',
+        builder: (context, state) => MoneyScreen(
+          estateStore: estateStore,
+          onBack: () { context.go('/'); },
+          onAddEstate: (estate) {
+            estateStore.add(estate);
+          },
+          onDeleteEstate: (id) {
+            estateStore.remove(id);
+          },
+          onEstateClick: (id) {},
+        ),
       ),
     ],
   );
